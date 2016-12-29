2f4ba5708664bd5960a25cd57d330aca69a41ace
infwonder <infwonder@gmail.com>
2016-12-29 16:41:04 -0700

    Updated with some things leared with week 4 contents ... still need work

--- contracts/SplitSend.sol
+++ contracts/SplitSend.sol
@@ -2,25 +2,39 @@ pragma solidity ^0.4.4;
 
 contract SplitSend {
     address owner;
+    bool locked;
 
     function SplitSend() payable {
         owner = msg.sender;
     }
 
-    function sendWei(address toWhom1, address toWhom2) payable returns (bool) {
+    modifier OwnerOnly() {
+      if (msg.sender != owner) throw;
+      _;
+    }
+
+    modifier NoReentrancy() {
+      if (locked) throw;
+      locked = true;
+      _;
+      locked = false;
+    }
+
+    function sendWei(address toWhom1, address toWhom2) payable OwnerOnly NoReentrancy returns (bool) {
         uint256 sendAmount;
-        sendAmount = address(this).balance;
+        sendAmount = msg.value;
 
-        if (sendAmount > 0) {
+        if ( sendAmount > 0 && sendAmount % 2 == 0 ) {
             return toWhom1.send(sendAmount / 2) && toWhom2.send(sendAmount / 2);
+        } else {
+            return false;
         }
     }
 
-    function killMe() {
-        if (msg.sender == owner) {
-            suicide(owner);
-        }
+    function killMe() OwnerOnly {
+      selfdestruct(owner);
     }
 
+    // fallback function 
     function () payable {}
 } 
